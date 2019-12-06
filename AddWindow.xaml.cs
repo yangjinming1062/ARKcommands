f75b3f7aee6ae3ed6b523e1f94dbf21e57938eb0
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2019-12-06 16:00:58 +0800

    Update

--- AddWindow.xaml.cs
+++ AddWindow.xaml.cs
@@ -59,7 +59,7 @@ namespace ARKcommands
             ARKCommand command = new ARKCommand()
             {
                 Name = txtName.Text.Trim(),
-                Command = txtCommand.Text.Trim() + weizhui.Text,
+                Command ="giveitem "+ txtCommand.Text.Trim() + weizhui.Text,
                 Type = cmbType.Text,
                 Map = Transmit(cmbMap.Text),
                 Special = cmbSP.Text
